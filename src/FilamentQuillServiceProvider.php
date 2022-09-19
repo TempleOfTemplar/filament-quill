3ddf4cf92c1cb3ee57ab970fde6ef0a7199bc945
TempleOfTemplar <imbalanced47@gmail.com>
2022-09-19 07:56:05 +0500

    fix?6

--- src/FilamentQuillServiceProvider.php
+++ src/FilamentQuillServiceProvider.php
@@ -8,36 +8,9 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FilamentQuillServiceProvider extends PackageServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        $package
-            ->name('filament-quill')
-            ->hasViews()
-            ->hasAssets();
-    }
+    public static string $name = 'filament-quill';
 
-    public function packageBooted()
-    {
-        if (class_exists(\Filament\FilamentServiceProvider::class)) {
-            Filament::serving(function () {
-                Filament::registerScripts($this->getScripts(), true);
-                Filament::registerStyles($this->getStyles());
-            });
-        }
-    }
-
-    public function getScripts(): array
-    {
-        return [
-            'filament-quill' => __DIR__ . '/../resources/dist/js/editor.js',
-        ];
-    }
-
-    public function getStyles(): array
-    {
-        return [
-            'filament-quill-core' => __DIR__ . '/../resources/dist/css/quill.core.css',
-            'filament-quill-snow' => __DIR__ . '/../resources/dist/css/quill.snow.css',
-        ];
-    }
+    protected array $beforeCoreScripts = [
+        'filament-quill' => __DIR__ . '/../dist/js/quill.min.js',
+    ];
 }
