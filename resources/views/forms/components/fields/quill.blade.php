38380ed8f70192b2e6cb2de39432d4d2ba6f23c2
TempleOfTemplar <imbalanced47@gmail.com>
2022-09-19 07:27:00 +0500

    fix?3

--- resources/views/forms/components/fields/quill.blade.php
+++ resources/views/forms/components/fields/quill.blade.php
@@ -13,54 +13,56 @@
         <div
                 x-data="{ state: $wire.entangle('{{ $getStatePath() }}'), initialized: false }"
                 x-init="(() => {
-                window.addEventListener('DOMContentLoaded', () => initQuill());
-                const initQuill = ()=>{
+    window.addEventListener('DOMContentLoaded', () => initQuill());
+    const initQuill = ()=>{
 
-                                var quill = new Quill(this.$quill, {
-                    theme: 'snow',
-                    modules: {
-                        imageUploader: {
-                            upload: (file) => {
-                                return new Promise((resolve) => {
-                                    this.$wire.upload(
-                                        `componentFileAttachments.${statePath}`,
-                                        file,
-                                        (uploadedFilename) => {
-                                            this.$wire
-                                                .getComponentFileAttachmentUrl(statePath)
-                                                .then((url) => {
-                                                    if (!url) {
-                                                        return resolve({
-                                                            success: 0,
-                                                        });
-                                                    }
-                                                    return resolve({
-                                                        success: 1,
-                                                        file: {
-                                                            url: url,
-                                                        },
-                                                    });
+        var quill = new Quill(this.$quill, {
+            theme: 'snow',
+            modules: {
+                imageUploader: {
+                    upload: (file) => {
+                        return new Promise((resolve) => {
+                            this.$wire.upload(
+                                `componentFileAttachments.${statePath}`,
+                                file,
+                                (uploadedFilename) => {
+                                    this.$wire
+                                        .getComponentFileAttachmentUrl(statePath)
+                                        .then((url) => {
+                                            if (!url) {
+                                                return resolve({
+                                                    success: 0,
                                                 });
-                                        }
-                                    );
-                                });
-                            },
-                        },
+                                            }
+                                            return resolve({
+                                                success: 1,
+                                                file: {
+                                                    url: url,
+                                                },
+                                            });
+                                        });
+                                }
+                            );
+                        });
                     },
-                });
-                Quill.register('modules/imageUploader', ImageUploader);
+                },
+            },
+        });
+        Quill.register('modules/imageUploader', ImageUploader);
         quill.setContents(this.state);
         this.instance = quill;
         quill.on('editor-change', function (eventName, ...args) {
-        if (eventName === 'text-change') {
-        // args[0] will be delta
-        console.log('args', args);
-        this.state = args[0];
-        } else if (eventName === 'selection-change') {
-        // args[0] will be old range
-        }
+            if (eventName === 'text-change') {
+                // args[0] will be delta
+                console.log('args', args);
+                this.state = args[0];
+            } else if (eventName === 'selection-change') {
+                // args[0] will be old range
+            }
         });
-            })()"
+
+    }
+})()"
                 x-cloak
                 wire:ignore
         >
