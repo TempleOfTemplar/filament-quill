36132791d9ae069e76d2c1c87ee330ebbbf48e97
TempleOfTemplar <imbalanced47@gmail.com>
2022-09-20 22:11:43 +0500

    -wrebuild7

--- resources/js/quill-editor.js
+++ resources/js/quill-editor.js
@@ -1,32 +1,32 @@
-import Snow from 'quill/themes/snow'
+// import Snow from 'quill/themes/snow'
 import Toolbar from 'quill/modules/toolbar'
-import Bold from 'quill/formats/bold'
-import Italic from 'quill/formats/italic'
-import Header from 'quill/formats/header'
-import Underline from 'quill/formats/underline'
-import ListItem from 'quill/formats/list'
-import Strike from 'quill/formats/strike'
-import Blockquote from 'quill/formats/blockquote'
-import CodeBlock from 'quill/formats/code'
-import Script from 'quill/formats/script'
-import Image from 'quill/formats/image'
-import Link from 'quill/formats/link'
-import Quill from "quill/core/quill";
+// import Bold from 'quill/formats/bold'
+// import Italic from 'quill/formats/italic'
+// import Header from 'quill/formats/header'
+// import Underline from 'quill/formats/underline'
+// import ListItem from 'quill/formats/list'
+// import Strike from 'quill/formats/strike'
+// import Blockquote from 'quill/formats/blockquote'
+// import CodeBlock from 'quill/formats/code'
+// import Script from 'quill/formats/script'
+// import Image from 'quill/formats/image'
+// import Link from 'quill/formats/link'
+import Quill from "quill";
 
 Quill.register({
     'modules/toolbar': Toolbar,
-    'themes/snow': Snow,
-    'formats/bold': Bold,
-    'formats/italic': Italic,
-    'formats/header': Header,
-    'formats/underline': Underline,
-    'formats/list': ListItem,
-    'formats/strike': Strike,
-    'formats/blockquote': Blockquote,
-    'formats/codeblock': CodeBlock,
-    'formats/script': Script,
-    'formats/image': Image,
-    'formats/link': Link,
+    // 'themes/snow': Snow,
+    // 'formats/bold': Bold,
+    // 'formats/italic': Italic,
+    // 'formats/header': Header,
+    // 'formats/underline': Underline,
+    // 'formats/list': ListItem,
+    // 'formats/strike': Strike,
+    // 'formats/blockquote': Blockquote,
+    // 'formats/codeblock': CodeBlock,
+    // 'formats/script': Script,
+    // 'formats/image': Image,
+    // 'formats/link': Link,
 });
 
 // Quill.register('modules/imageUploader', ImageUploader);
