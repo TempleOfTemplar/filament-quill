e40da75c77f498a73ca2252b2d4e149628ce2229
TempleOfTemplar <imbalanced47@gmail.com>
2022-09-21 08:45:12 +0500

    -wrebuild11

--- resources/js/quill-editor.js
+++ resources/js/quill-editor.js
@@ -1,41 +1,4 @@
-// import Snow from 'quill/themes/snow'
-// import Bold from 'quill/formats/bold'
-// import Italic from 'quill/formats/italic'
-// import Header from 'quill/formats/header'
-// import Underline from 'quill/formats/underline'
-// import ListItem from 'quill/formats/list'
-// import Strike from 'quill/formats/strike'
-// import Blockquote from 'quill/formats/blockquote'
-// import CodeBlock from 'quill/formats/code'
-// import Script from 'quill/formats/script'
-// import Image from 'quill/formats/image'
-// import Link from 'quill/formats/link'
-import Quill from "quill/core";
-import Toolbar from 'quill/modules/toolbar';
-import Snow from 'quill/themes/snow';
-
-import Bold from 'quill/formats/bold';
-import Italic from 'quill/formats/italic';
-import Header from 'quill/formats/header';
-
-Quill.register({
-    'modules/toolbar': Toolbar,
-    'themes/snow': Snow,
-    'formats/bold': Bold,
-    'formats/italic': Italic,
-    'formats/header': Header,
-    // 'formats/underline': Underline,
-    // 'formats/list': ListItem,
-    // 'formats/strike': Strike,
-    // 'formats/blockquote': Blockquote,
-    // 'formats/codeblock': CodeBlock,
-    // 'formats/script': Script,
-    // 'formats/image': Image,
-    // 'formats/link': Link,
-});
-
-// Quill.register('modules/imageUploader', ImageUploader);
-
+import Quill from "./quill-exported";
 
 export default (Alpine) => {
     Alpine.data('quilleditor', ({state, statePath, placeholder, readOnly, id, tools, minHeight}) => ({
